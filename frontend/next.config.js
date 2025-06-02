739d5b3660f125b758f890baea5aa3a79276a4f4
OnTrak-Tech <adjeigideon911@gmail.com>
2025-06-02 00:22:59 +0000

    Edited some Frontend files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
   images: {
     unoptimized: true,
     // Use hardcoded values instead of environment variables
-    domains: ['taskbuddy-dev-attachments.s3.amazonaws.com'],
   },
-  // Add this to ensure proper static export
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
