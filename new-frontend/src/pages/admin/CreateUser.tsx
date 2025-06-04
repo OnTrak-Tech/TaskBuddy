de2066daf20552e1aca515694a1e801aecca3311
OnTrak-Tech <adjeigideon911@gmail.com>
2025-06-04 22:39:27 +0000

    Fixed

--- new-frontend/src/pages/admin/CreateUser.tsx
+++ new-frontend/src/pages/admin/CreateUser.tsx
@@ -2,7 +2,6 @@ import { useState, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from '../../components/ui/Button'
 import Card from '../../components/ui/Card'
-import { useApi } from '../../hooks/useApi'
 import cognitoAdmin from '../../lib/cognitoAdmin'
 
 const CreateUser = () => {
